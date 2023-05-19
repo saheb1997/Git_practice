81cd589e36ec384c729ec5c21765630e7cb46f1f
Your name <sahebs450@gmail.com>
2023-05-19 10:55:44 +0530

    first commit

---
+++ first.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+    {
+        cout<<"saheb sarkar\n";
+        cout<<"enter value of a and b";
+        int a,b;
+        cin>>b>>a;
+        cout<<a+b;
+
+    }
